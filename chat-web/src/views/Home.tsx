b0e0fda15adf1e8b816d5606d8898220e4adebfc
ylc <756885686@qq.com>
2025-02-12 10:53:07 +0800

    feat：修改文案

--- chat-web/src/views/Home.tsx
+++ chat-web/src/views/Home.tsx
@@ -131,7 +131,7 @@ const Home: React.FC = () => {
             />
           </Tooltip>
           <div className="logo">
-            <span>🐕Chat-GPT🐕</span>
+            <span>小金-AI</span>
           </div>
         </Header>
         <Content style={{ margin: "10px 10px 0", height: contentHeight, overflowY: "auto" }}>
